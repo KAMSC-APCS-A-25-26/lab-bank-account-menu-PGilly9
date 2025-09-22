23dbfa88c78450dcaeaa5263176d707c0b5b0806
PGilly9 <parker.gilliland2009@gmail.com>
2025-09-22 13:46:15 -0400

    Update BankAccountMenu.java
    This program acts like a bank.

--- src/main/java/BankAccountMenu.java
+++ src/main/java/BankAccountMenu.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class BankAccountMenu {
     public static void main(String[] args) {
         // TODO: Implement the bank account menu
@@ -9,5 +8,42 @@ public class BankAccountMenu {
         // 4. Use Scanner to read user input
         // 5. Use switch statement to handle menu choices
         // 6. Implement add money, withdraw money, check balance, and exit functionality
+        Scanner scan = new Scanner(System.in);
+        boolean run = true;
+        int account = 0;
+        while (run) {
+            System.out.println("\n");
+            System.out.println("1. Add Money");
+            System.out.println("2. Withdraw Money");
+            System.out.println("3. Check Balance");
+            System.out.println("4. Exit");
+            System.out.print("Enter Your Choice:  ");
+            int input = scan.nextInt();
+            System.out.print("\n");
+
+            switch (input) {
+                case 1:
+                    System.out.println("How Much:  ");
+                    int money = scan.nextInt();
+                    account = account + money;
+                    break;
+
+                case 2:
+                    System.out.println("How Much:  ");
+                    money = scan.nextInt();
+                    account = account - money;
+                    break;
+
+                case 3:
+                    System.out.println("Your balance is " + account);
+                    break;
+
+                case 4:
+                    run = false;
+                    System.out.println("Goodbye!");
+                    break;
+            }
+
+        }
     }
 }
